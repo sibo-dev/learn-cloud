34b7f001c243cc11cfe6391a962a721419b04cae
sibo-dev <sibonelo.dev@gmail.com>
2023-08-28 10:46:55 +0200

    Undo secrets

--- aws-cloud/instances/main.tf
+++ aws-cloud/instances/main.tf
@@ -94,7 +94,7 @@ resource "aws_security_group" "sg_22_80" {
 }
 
 data "template_file" "user_data" {
-  template = file("../secrets/add-ssh-web-app.yaml")
+  template = file("../scripts/add-ssh-web-app.yaml")
 }
 
 resource "aws_instance" "web" {
